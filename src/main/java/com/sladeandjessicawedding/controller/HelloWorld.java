817094c3b0644b6a07719d6bbfde52fa6e275943
Slade Baumann <slade.baumann@target.com>
2018-11-15 11:47:46 -0600

    Fix hello world

--- src/main/java/com/sladeandjessicawedding/controller/HelloWorld.java
+++ src/main/java/com/sladeandjessicawedding/controller/HelloWorld.java
@@ -1,13 +1,14 @@
 package com.sladeandjessicawedding.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorld {
 
     @GetMapping("/helloworld")
-    public String helloworld() {
-        return "Hello World";
+    public String helloworld(Model model) {
+        return "helloworld";
     }
 }
