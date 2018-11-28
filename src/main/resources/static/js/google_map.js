0b743e1e77441ff8287267ec2b1c39856147dd91
Slade Baumann <slade.baumann@target.com>
2018-11-28 14:02:55 -0600

    Fix google maps

--- src/main/resources/static/js/google_map.js
+++ src/main/resources/static/js/google_map.js
@@ -4,29 +4,8 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var latLngSunkenGarden = new google.maps.LatLng(44.944376, -93.126084);
-    var latLngOakRoom = new google.maps.LatLng(44.981849, -93.150775);
-    
-    var mapOptionsSunkenGarden = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-        // The latitude and longitude to center the map (always required)
-        center: latLngSunkenGarden,
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
-    };
-    var mapOptionsOakRoom = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-        // The latitude and longitude to center the map (always required)
-        center: latLngOakRoom,
-        // How you would like to style the map.
-        scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
-    };
-
-    
+    var latLngSunkenGarden = {lat: 44.944260, lng: -93.126082};
+    var latLngOakRoom = {lat: 44.981804, lng: -93.150834};
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
@@ -34,23 +13,28 @@ function init() {
     var mapElementOakRoom = document.getElementById('map-oak-room');
 
     // Create the Google Map using out element and options defined above
-    var mapSunkenGarden = new google.maps.Map(mapElementSunkenGarden, mapOptionsSunkenGarden);
-    var mapOakRoom = new google.maps.Map(mapElementOakRoom, mapOptionsOakRoom);
-    
-    // var addresses = ['Sunken Garden', 'Oak Room'];
-    //
-    // for (var x = 0; x < addresses.length; x++) {
-    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-    //         var p = data.results[0].geometry.location
-    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
-    //         new google.maps.Marker({
-    //             position: latlng,
-    //             map: map,
-    //             icon: 'images/loc.png'
-    //         });
-    //
-    //     });
-    // }
-    
+    var mapSunkenGarden = new google.maps.Map(mapElementSunkenGarden, {zoom: 11, center: latLngSunkenGarden});
+    var mapOakRoom = new google.maps.Map(mapElementOakRoom, {zoom: 11, center: latLngOakRoom});
+
+    // Create markers
+    var markerSunkenGarden = new google.maps.Marker({position: latLngSunkenGarden, map: mapSunkenGarden, url: 'https://maps.google.com/maps/?q=44.944260,-93.126082'});
+    var markerOakRoom = new google.maps.Marker({position: latLngOakRoom, map: mapOakRoom, url: 'https://maps.google.com/maps/?q=44.981804,-93.150834'});
+
+    markerSunkenGarden.addListener('click', function() {
+        if (this.url) {
+            // document.location = this.url;
+            window.open(this.url, '_blank');
+            // Or like this if you want a new tab : window.open(this.url, '_blank');
+        }
+    });
+    markerOakRoom.addListener('click', function() {
+        if (this.url) {
+            // document.location = this.url;
+            window.open(this.url, '_blank');
+            // Or like this if you want a new tab : window.open(this.url, '_blank');
+        }
+    });
+
 }
+
 google.maps.event.addDomListener(window, 'load', init);
