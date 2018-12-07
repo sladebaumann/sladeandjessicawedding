d178a57042ec5389b2ee883a7d94fc69869d6bb2
Slade Baumann <slade.baumann@target.com>
2018-12-07 11:33:29 -0600

    Update database and add a few pictures

--- scripts/init.sql
+++ scripts/init.sql
@@ -10,3 +10,13 @@ CREATE TABLE "wedding"."guest_list" (
   "notes" TEXT NULL,
   PRIMARY KEY ("guest_id")
 );
+
+CREATE TABLE "wedding"."music_request" (
+  "request_id" SERIAL8 NOT NULL,
+  "guest_id" INT8 NOT NULL,
+  "name" VARCHAR(50) NOT NULL,
+  "artist" VARCHAR(50) NULL,
+  PRIMARY KEY ("request_id")
+);
+
+ALTER TABLE "wedding"."music_request" ADD CONSTRAINT "fk_music_request_guest_id" FOREIGN KEY ("guest_id") REFERENCES "wedding"."guest_list"("guest_id");
